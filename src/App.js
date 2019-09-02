c6ecb65bde9fa3c9a813b7f79d3b49595c8c2bd8
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-09-02 16:36:50 -0400

    added some buttons and made app the parent component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any change handlers you need to work with your state  
+  constructor(){
+    super();
+    this.state = {
+      name:'Connor'
+    }
+      //Put event handlers in constructor method then bind them in the JSX
+
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List:</h1>
+        <br></br>
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TodoForm extends React.Component{
+    
+
+    render(){
+        return(
+            <div>
+                <button> ADD to list </button>
+                <button> CLEAR COMPLETED </button>
+            </div>
+        )
+    }
+    
+}
+
+export default TodoForm;
